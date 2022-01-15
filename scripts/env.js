805fd41689979bf1cbcfdde1d205d78c478f1468
MrBBot <me@mrbbot.dev>
2022-01-15 19:05:09 +0000

    Log `env.jsonc` parsing errors instead of silently catching

--- scripts/env.js
+++ scripts/env.js
@@ -21,16 +21,14 @@ const envPath = path.resolve(__dirname, "..", "env.jsonc");
  * @property {[Env~Application]} production
  */
 
+const envJson = (await fs.readFile(envPath, "utf8"))
+  .split("\n")
+  // Remove "//" comments
+  .map((line) => line.replace(/\/\/.*$/, "").trim())
+  .filter(Boolean)
+  .join("\n");
+
 /**
  * @type {Env}
  */
-export let env = {};
-try {
-  const envJson = (await fs.readFile(envPath, "utf8"))
-    .split("\n")
-    // Remove "//" comments
-    .map((line) => line.replace(/\/\/.*$/, "").trim())
-    .filter(Boolean)
-    .join("\n");
-  env = JSON.parse(envJson);
-} catch {}
+export const env = JSON.parse(envJson);
